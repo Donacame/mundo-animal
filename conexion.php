e964145af9c561057e33cca6c6af25674338f898
Donacame <dorelyscmedina@gmail.com>
2026-07-10 02:09:08 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,4 +1,5 @@
 <?php
+<?php
 $host = getenv('DB_HOST');
 $port = getenv('DB_PORT');
 $user = getenv('DB_USER');
@@ -7,10 +8,10 @@ $db   = getenv('DB_NAME');
 
 $conn = pg_connect("host=$host port=$port dbname=$db user=$user password=$pass");
 
-// Verificar conexion
 if (!$conn) {
     die("Error de conexión: " . pg_last_error());
 }
+?>
 
 // Asegurar UTF-8 para acentos y ñ
 $conexion->set_charset("utf8");
